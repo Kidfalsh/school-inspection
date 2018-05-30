ed9663658ed90e3914e4cbe401c484c2c0e246bb
unknown <theflashdye@163.com>
2018-05-30 15:30:15 +0800

    修改iphone机上无法显示地图问题

--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -2,9 +2,9 @@
 <template>
   <div class="test">
     <my-header :title="'请假审批'"></my-header>
-    <baidu-map class="map" :center="{lng: longitude, lat: latitude}" :zoom="15">
-    <bm-marker :position="{lng: longitude, lat: latitude}" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
-      <bm-label content="你在这儿" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/>
+    <baidu-map class="map" :center="center" :zoom="zoom" @ready="handler">
+    <bm-marker :position="autoLocationPoint" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
+      <bm-label content="你在这儿" :labelStyle="{color: 'red', fontSize : '12px'}" :offset="{width: -25, height: 30}"/>
     </bm-marker>
     </baidu-map>
 
@@ -16,8 +16,10 @@ import myHeader from "@/components/header/header.vue";
 export default {
   data() {
     return {
-      longitude:'',//维度
-      latitude:'',//经度
+      center:'',
+      zoom:15,
+      autoLocationPoint:'',
+      initLocation:''
     }
   },
   computed: {},
@@ -25,28 +27,20 @@ export default {
     this.$store.commit('setPageTitle','请假审批')
   },
   mounted() {
-    this.getLocation()
   },
   methods: {
-    getLocation(){
-      if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          position=>{  
-            this.longitude = position.coords.longitude;  
-            this.latitude = position.coords.latitude;  
-            console.log(longitude)
-            console.log(latitude)
-          },
-          err=>{
-            console.log(err)
-            var msg = err.code;
-            var dd = err.message;
-            console.log(msg)
-            console.log(dd)
-          }
-        ) 
-      }
-    }
+    handler({BMap,map}){
+      let _this = this;   // 设置一个临时变量指向vue实例，因为在百度地图回调里使用this，指向的不是vue实例；
+      var geolocation = new BMap.Geolocation();
+      geolocation.getCurrentPosition(function(r){
+        console.log(r)
+        // 如果这里直接使用this是不行的
+        _this.center = {lng: r.longitude, lat: r.latitude};     // 设置center属性值
+        _this.autoLocationPoint = {lng: r.longitude, lat: r.latitude};      // 自定义覆盖物
+        _this.initLocation = true; 
+        //_this.zoom = 15;    
+      },{enableHighAccuracy: true})
+    },
   },
   components: {
     myHeader
